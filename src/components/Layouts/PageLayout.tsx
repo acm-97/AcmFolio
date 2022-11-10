481705f3cdb16f0a081e062905f4f8de88ae7a33
acm9711 <alejandro@weweb.com>
2022-11-10 09:07:50 +0100

    add head data to layout

--- src/components/Layouts/PageLayout.tsx
+++ src/components/Layouts/PageLayout.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { MainLayout, MainLayoutRootProps } from '@/components/Layouts';
 import PageWidthContainer from '@/components/Layouts/PageWidthContainer';
 import { Box } from '@mui/material';
-import { MainLayoutRootProps, MainLayout } from '@/components/Layouts';
+import Head from 'next/head';
 
 const PageLayout = ({ children }: MainLayoutRootProps) => (
   <MainLayout>
     <PageWidthContainer>
+      <Head>
+        <title>ACM</title>
+        <meta name="description" content="Personal Portafolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Box mb={8}>{children}</Box>
     </PageWidthContainer>
   </MainLayout>
