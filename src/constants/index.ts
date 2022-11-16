08a4b7bb0aaaafb73d15e41ec547eee3906769bb
alejandro <alejandro97.acm@gmail.com>
2022-11-16 10:13:05 -0500

    update template

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,7 @@ export { systemCommands, columns } from './systemCommands';
 
 export const GITHUB = process.env.NEXT_PUBLIC_GITHUB;
 export const LINKEDIN = process.env.NEXT_PUBLIC_LINKEDIN;
+export const INSTAGRAM = process.env.NEXT_PUBLIC_INSTAGRAM;
 export const ES_FRONTEND_DEVELOPER =
   process.env.NEXT_PUBLIC_ES_FRONTEND_DEVELOPER;
 export const EN_FRONTEND_DEVELOPER =
