2dff4e40dca3e1f5be7edc8d4853523f95ecafd1
acm9711 <alejandro@weweb.com>
2022-11-17 12:12:04 +0100

    add dragable context and hooks

---
+++ src/contexts/DraggableContext.tsx
@@ -0,0 +1,108 @@
+import {
+  createContext,
+  ReactNode,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+
+export type DraggProps = {
+  projectName: string;
+  project: ProjectsProps;
+  top: number;
+  left: number;
+};
+
+export type ProjectsProps = {
+  id: string;
+  name: string;
+  fullName: string;
+  url: string;
+  description: string;
+  owner: {
+    id: string;
+    avatar_url: string;
+  };
+};
+
+interface DraggableContextProps {
+  draggables: DraggProps[];
+  saveDraggable: (draggable: DraggProps) => void;
+  closeDragable: (draggable: DraggProps[]) => void;
+  projects: ProjectsProps[];
+}
+
+export const initialDraggable: DraggProps[] = [];
+
+export const DraggableContext = createContext<DraggableContextProps>({
+  draggables: initialDraggable,
+  // eslint-disable-next-line
+  saveDraggable: (Draggable: DraggProps) => {},
+  closeDragable: (Draggable: DraggProps[]) => {},
+  projects: [],
+});
+
+type DraggableProviderProps = {
+  children: ReactNode;
+};
+
+const DraggableProvider = ({ children }: DraggableProviderProps) => {
+  const [draggables, setDraggable] = useState<DraggProps[]>(initialDraggable);
+  const [projects, setProjects] = useState<ProjectsProps[]>([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    const fetchProjects = () => {
+      fetch('https://api.github.com/users/acm-97/repos', { signal })
+        .then((res) => res.json())
+        .then((repos) => {
+          const result = repos.map((item: any) => ({
+            id: item.id,
+            name: item.name,
+            fullName: item.full_name,
+            url: item.html_url,
+            description: item.description,
+            owner: {
+              id: item.owner.id,
+              avatar_url: item.owner.avatar_url,
+            },
+          }));
+
+          setProjects(result);
+        })
+        .catch((err) => {
+          if (err.name === 'AbortError') console.log('previus fetch canceled');
+          else console.log(err);
+        });
+    };
+    fetchProjects();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  const saveDraggable = (newDraggable: DraggProps) =>
+    setDraggable((prev) => [...prev, newDraggable]);
+
+  const closeDragable = (newDraggables: DraggProps[]) =>
+    setDraggable(newDraggables);
+
+  return (
+    <DraggableContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        draggables,
+        saveDraggable,
+        closeDragable,
+        projects,
+      }}
+    >
+      {children}
+    </DraggableContext.Provider>
+  );
+};
+export const useDraggable = () => useContext(DraggableContext);
+
+export default DraggableProvider;
---
+++ src/hooks/useDraggablePreviews.tsx
@@ -0,0 +1,39 @@
+import { useMemo, useCallback } from 'react';
+
+import { ProjectsProps, useDraggable } from '@/contexts/DraggableContext';
+
+export type newDraggableProps = {
+  projectName: string;
+  project: ProjectsProps;
+};
+
+const useDraggablePreviews = () => {
+  const { projects, saveDraggable, draggables } = useDraggable();
+
+  const lastDraggableTop = useMemo(
+    () =>
+      draggables.length > 0 ? draggables[draggables.length - 1].top + 40 : 0,
+
+    [draggables]
+  );
+
+  const handlePreviews = useCallback(
+    (newDraggable: newDraggableProps) => {
+      const dragg = draggables.find(
+        (item) => item.projectName === newDraggable.projectName
+      );
+      if (!dragg) {
+        saveDraggable({
+          ...newDraggable,
+          top: lastDraggableTop,
+          left: 0,
+        });
+      }
+    },
+    [draggables, lastDraggableTop, saveDraggable]
+  );
+
+  return { projects, lastDraggableTop, handlePreviews };
+};
+
+export default useDraggablePreviews;
