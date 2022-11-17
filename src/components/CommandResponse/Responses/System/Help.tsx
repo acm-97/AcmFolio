e333e22c3ccddc4f6938d0115e4c189d480f1ad0
acm9711 <alejandro@weweb.com>
2022-11-17 12:43:21 +0100

    change overflowX styles

--- src/components/CommandResponse/Responses/System/Help.tsx
+++ src/components/CommandResponse/Responses/System/Help.tsx
@@ -12,6 +12,25 @@ const Help = ({ rows, columns }: HelpProps) => (
   <Box
     sx={{
       margin: '20px 0',
+      '& .MuiTableContainer-root': {
+        /* width */
+        '&::-webkit-scrollbar': {
+          width: '8px',
+          height: '10px',
+        },
+
+        /* Track */
+        '&::-webkit-scrollbar-track': {
+          boxShadow: (theme) => `inset 0 0 4px ${theme.palette.secondary.main}`,
+          borderColor: (theme) => theme.palette.secondary.main,
+          borderRadius: '15px',
+        },
+        /* Handle */
+        '&::-webkit-scrollbar-thumb': {
+          background: '#B9BBC6',
+          borderRadius: '15px',
+        },
+      },
       '&  .MuiTableRow-root ': {
         '&:nth-of-type(even)': {
           backgroundColor: (theme) => theme.palette.action.hover,
