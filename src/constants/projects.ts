64cec3849032069401a4ec8424eeced5cd576014
acm9711 <alejandro@weweb.com>
2022-11-17 12:09:24 +0100

    fix projects images constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,7 @@
+export const images: { [x: string]: string } = {
+  'JavaScript-Challenges': '/javascript-challenges.png',
+
+  'portfolio-v1': '/portfolio-v1.png',
+
+  'portfolio-v2': '/portfolio-v2.png',
+};
